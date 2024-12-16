c6884f12a4d6b8e7dbddf54f0974bd75d705836f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-12-16 11:54:10 +0100

    Add MSAA example

--- examples/core_msaa.c
+++ examples/core_msaa.c
@@ -1,5 +1,16 @@
 #include <raygpu.h>
 
-int main(cwoid){
+int main(void){
     SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(800, 600, "WebGPU window");
+    bool flag = 0;
+    const int textWidth = MeasureText("Hello VSync Enjoyer", 100);
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground((Color){130, 130, 130, 255});
+        DrawText("Hello VSync Enjoyer", GetScreenWidth() / 2 - textWidth / 2, GetScreenHeight() / 2 - 50, 100, (Color){210, 210, 210,255});
+        DrawCircleSector(GetMousePosition(), 100.0f, 0.0f, 360.0f, 128, WHITE);
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
 }
