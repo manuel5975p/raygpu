b2daf0aa4863992d216ed736bcb1cfb8f92840a1
manuel5975p <manuel.winkler@gmx.ch>
2024-12-20 01:37:41 +0100

    wasm

--- examples/core_window.c
+++ examples/core_window.c
@@ -1,12 +1,22 @@
 #include <raygpu.h>
-
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+void mainloop(){
+    BeginDrawing();
+    ClearBackground((Color){230, 230, 230,255});
+    DrawText("Hello WebGPU enjoyer", 100, 300, 50, (Color){190, 190, 190,255});
+    EndDrawing();
+}
 int main(void){
-    InitWindow(800, 600, "WebGPU window");
-    
+    SetConfigFlags(FLAG_VSYNC_HINT);
+    InitWindow(1920, 1080, "WebGPU window");
+    #ifndef __EMSCRIPTEN__
     while(!WindowShouldClose()){
-        BeginDrawing();
-        ClearBackground((Color){230, 230, 230,255});
-        DrawText("Hello WebGPU enjoyer", 100, 300, 50, (Color){190, 190, 190,255});
-        EndDrawing();
+        mainloop();
     }
+    #else
+    emscripten_set_main_loop(mainloop, 0, 0);
+    #endif
+    
 }
