2c4e01875c94b3d3baf8a7156c2c429ba941dc81
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-01-15 18:14:59 +0100

    sdl/browser togglefullscreen is weird

--- examples/core_resizable.c
+++ examples/core_resizable.c
@@ -1,17 +1,21 @@
 #include <raygpu.h>
-
+void mainloop(void){
+    BeginDrawing();
+    ClearBackground((Color){230, 230, 230,255});
+    DrawText("Hello WebGPU enjoyer", 100, 300, 50, (Color){190, 190, 190,255});
+    if(IsKeyPressed(KEY_U)){
+        ToggleFullscreen();
+    }
+    EndDrawing();
+}
 int main(void){
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow(800, 600, "WebGPU window");
-    
+    #ifndef __EMSCRIPTEN__
     while(!WindowShouldClose()){
-        BeginDrawing();
-        ClearBackground((Color){230, 230, 230,255});
-        DrawText("Hello WebGPU enjoyer", 100, 300, 50, (Color){190, 190, 190,255});
-        if(IsKeyPressed(KEY_U)){
-            ToggleFullscreen();
-        }
-        EndDrawing();
-        
+        mainloop();
     }
+    #else
+    emscripten_set_main_loop(mainloop, 0, 0);
+    #endif
 }
--- examples/core_window.c
+++ examples/core_window.c
@@ -22,6 +22,7 @@ int main(void){
     RequestLimit(maxBufferSize, 1 << 13);
     RequestBackend(WGPUBackendType_Vulkan);
     //SetConfigFlags(FLAG_VSYNC_LOWLATENCY_HINT);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 600, "WebGPU window");
     //SetTargetFPS(0);
     //sdlwin = OpenSubWindow_GLFW(400, 400, "SDL fenschter");
