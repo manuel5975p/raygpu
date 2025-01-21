abd8ed5811abfe5a26305fa5032c10dc88fe9c90
manuel5975p <manuel.winkler@gmx.ch>
2025-01-21 11:57:59 +0100

    Vulkan

--- examples/core_window.c
+++ examples/core_window.c
@@ -10,7 +10,7 @@ void mainloop(void){
     //EndWindowMode();
 
     BeginDrawing();
-    ClearBackground((Color){230, 230, 230,255});
+    ClearBackground((Color){230, 23, 230,255});
     DrawText("Hello WebGPU enjoyer", 100, 300, 50, (Color){190, 190, 190,255});
     DrawFPS(5, 5);
     if(IsKeyPressed(KEY_U)){
