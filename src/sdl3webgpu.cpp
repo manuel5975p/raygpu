325f2fd9f07954288c883a537a807aadaa18c246
manuel <manuel.winkler@gmx.ch>
2025-03-07 10:28:10 +0100

    Fix sdl3 + wgpu with x11

--- src/sdl3webgpu.cpp
+++ src/sdl3webgpu.cpp
@@ -10,7 +10,7 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_video.h>
 //#include <SDL3/SDL_properties.h>
-#if USE_X11 == 1
+#if RAYGPU_USE_X11 == 1
 #include <X11/Xlib.h>
 #endif
 #if defined(SDL_VIDEO_DRIVER_COCOA)
