c0557e58d8b0387b87763238e0d73a5042cb8ba9
manuel <manuel.winkler@gmx.ch>
2025-05-26 19:18:17 +0200

    Tint reflection

--- src/backend_vulkan/tint_c_api.cpp
+++ src/backend_vulkan/tint_c_api.cpp
@@ -1,3 +1,4 @@
+#include "src/tint/lang/core/type/reference.h"
 #include "src/tint/lang/wgsl/ast/identifier_expression.h"
 #include "src/tint/lang/wgsl/ast/templated_identifier.h"
 #include "src/tint/lang/wgsl/ast/var.h"
@@ -27,43 +28,44 @@ struct VarVisibility {
     ShaderStageMask visibilty;
 };
 
-
-format_or_sample_type extractFormat(const tint::ast::Identifier* iden){
-    if(auto templiden = iden->As<tint::ast::TemplatedIdentifier>()){
-        for(size_t i = 0;i < templiden->arguments.Length();i++){
-            if(templiden->arguments[i]->As<tint::ast::IdentifierExpression>() && templiden->arguments[i]->As<tint::ast::IdentifierExpression>()->identifier){
+format_or_sample_type extractFormat(const tint::ast::Identifier *iden) {
+    if (auto templiden = iden->As<tint::ast::TemplatedIdentifier>()) {
+        for (size_t i = 0; i < templiden->arguments.Length(); i++) {
+            if (templiden->arguments[i]->As<tint::ast::IdentifierExpression>() &&
+                templiden->arguments[i]->As<tint::ast::IdentifierExpression>()->identifier) {
                 auto arg_iden = templiden->arguments[i]->As<tint::ast::IdentifierExpression>()->identifier;
-                if(arg_iden->symbol.Name() == "r32float"){
+                if (arg_iden->symbol.Name() == "r32float") {
                     return format_r32float;
-                }
-                else if(arg_iden->symbol.Name() == "r32uint"){
+                } else if (arg_iden->symbol.Name() == "r32uint") {
                     return format_r32uint;
-                }
-                else if(arg_iden->symbol.Name() == "rgba8unorm"){
+                } else if (arg_iden->symbol.Name() == "rgba8unorm") {
                     return format_rgba8unorm;
-                }
-                else if(arg_iden->symbol.Name() == "rgba32float"){
+                } else if (arg_iden->symbol.Name() == "rgba32float") {
                     return format_rgba32float;
                 }
-                if(arg_iden->symbol.Name() == "f32"){
+                if (arg_iden->symbol.Name() == "f32") {
                     return sample_f32;
-                }
-                else if(arg_iden->symbol.Name() == "u32"){
+                } else if (arg_iden->symbol.Name() == "u32") {
                     return sample_u32;
                 }
-                //TODO: support all, there's not that many
+                // TODO: support all, there's not that many
             }
         }
     }
     return format_or_sample_type(12123);
 }
-static inline WGPUTextureFormat toStorageTextureFormat(format_or_sample_type fmt){
-    switch(fmt){
-        case format_or_sample_type::format_r32float: return WGPUTextureFormat_R32Float;
-        case format_or_sample_type::format_r32uint: return WGPUTextureFormat_R32Uint;
-        case format_or_sample_type::format_rgba8unorm: return WGPUTextureFormat_RGBA8Unorm;
-        case format_or_sample_type::format_rgba32float: return WGPUTextureFormat_RGBA32Float;
-        default: rg_unreachable();
+static inline WGPUTextureFormat toStorageTextureFormat(format_or_sample_type fmt) {
+    switch (fmt) {
+    case format_or_sample_type::format_r32float:
+        return WGPUTextureFormat_R32Float;
+    case format_or_sample_type::format_r32uint:
+        return WGPUTextureFormat_R32Uint;
+    case format_or_sample_type::format_rgba8unorm:
+        return WGPUTextureFormat_RGBA8Unorm;
+    case format_or_sample_type::format_rgba32float:
+        return WGPUTextureFormat_RGBA32Float;
+    default:
+        rg_unreachable();
     }
     return WGPUTextureFormat_Force32;
 }
@@ -92,7 +94,7 @@ RGAPI WGPUReflectionInfo reflectionInfo_wgsl_sync(WGPUStringView wgslSource) {
             }
         }
 
-        ret.globals = (WGPUGlobalReflectionInfo*)RL_CALLOC(globalsByName.size(), sizeof(WGPUGlobalReflectionInfo));
+        ret.globals = (WGPUGlobalReflectionInfo *)RL_CALLOC(globalsByName.size(), sizeof(WGPUGlobalReflectionInfo));
         uint32_t globalInsertIndex = 0;
         for (const auto &[name, varvis] : globalsByName) {
 
@@ -103,14 +105,16 @@ RGAPI WGPUReflectionInfo reflectionInfo_wgsl_sync(WGPUStringView wgslSource) {
             insert.binding = sem->Attributes().binding_point->binding;
             insert.name = WGPUStringView{name.c_str(), name.size()};
             insert.visibility = varvis.visibilty;
-            
+
             auto iden = varvis.var->type->As<tint::ast::IdentifierExpression>()->identifier;
 
             if (iden->symbol.Name().starts_with("texture_2d")) {
                 format_or_sample_type sampletype = extractFormat(iden);
-                assert(sampletype == format_or_sample_type::sample_f32 || sampletype == format_or_sample_type::sample_u32 && "Invalid format or sample type");
-                insert.texture.sampleType = (sampletype == format_or_sample_type::sample_f32) ? WGPUTextureSampleType_Float : WGPUTextureSampleType_Uint;
-                
+                assert(sampletype == format_or_sample_type::sample_f32 ||
+                       sampletype == format_or_sample_type::sample_u32 && "Invalid format or sample type");
+                insert.texture.sampleType =
+                    (sampletype == format_or_sample_type::sample_f32) ? WGPUTextureSampleType_Float : WGPUTextureSampleType_Uint;
+
                 if (iden->symbol.Name().starts_with("texture_2d_array")) {
                     insert.texture.viewDimension = WGPUTextureViewDimension_2DArray;
                 } else {
@@ -119,20 +123,30 @@ RGAPI WGPUReflectionInfo reflectionInfo_wgsl_sync(WGPUStringView wgslSource) {
             }
             if (iden->symbol.Name().starts_with("texture_3d")) {
                 format_or_sample_type sampletype = extractFormat(iden);
-                insert.texture.sampleType = (sampletype == format_or_sample_type::sample_f32) ? WGPUTextureSampleType_Float : WGPUTextureSampleType_Uint;
+                insert.texture.sampleType =
+                    (sampletype == format_or_sample_type::sample_f32) ? WGPUTextureSampleType_Float : WGPUTextureSampleType_Uint;
                 insert.texture.viewDimension = WGPUTextureViewDimension_3D;
             }
             if (iden->symbol.Name().starts_with("texture_storage_2d")) {
                 format_or_sample_type sampletype = extractFormat(iden);
                 insert.storageTexture.format = toStorageTextureFormat(sampletype);
-                
-                if(varvis.var->As<tint::ast::Var>()->declared_address_space->As<tint::ast::IdentifierExpression>()->identifier->symbol.NameView().starts_with("read_write"))
+
+                if (varvis.var->As<tint::ast::Var>()
+                        ->declared_access->As<tint::ast::IdentifierExpression>()
+                        ->identifier->symbol.NameView()
+                        .starts_with("read_write"))
                     insert.storageTexture.access = WGPUStorageTextureAccess_ReadWrite;
-                else if(varvis.var->As<tint::ast::Var>()->declared_address_space->As<tint::ast::IdentifierExpression>()->identifier->symbol.NameView().starts_with("readonly"))
+                else if (varvis.var->As<tint::ast::Var>()
+                             ->declared_access->As<tint::ast::IdentifierExpression>()
+                             ->identifier->symbol.NameView()
+                             .starts_with("readonly"))
                     insert.storageTexture.access = WGPUStorageTextureAccess_ReadOnly;
-                else if(varvis.var->As<tint::ast::Var>()->declared_address_space->As<tint::ast::IdentifierExpression>()->identifier->symbol.NameView().starts_with("writeonly"))
+                else if (varvis.var->As<tint::ast::Var>()
+                             ->declared_access->As<tint::ast::IdentifierExpression>()
+                             ->identifier->symbol.NameView()
+                             .starts_with("writeonly"))
                     insert.storageTexture.access = WGPUStorageTextureAccess_WriteOnly;
-                else{
+                else {
                     assert(false);
                 }
 
@@ -142,18 +156,168 @@ RGAPI WGPUReflectionInfo reflectionInfo_wgsl_sync(WGPUStringView wgslSource) {
                     insert.storageTexture.viewDimension = WGPUTextureViewDimension_2D;
                 }
 
-            }
-            if (iden->symbol.Name().starts_with("texture_storage_3d")) {
+            } else if (iden->symbol.Name().starts_with("texture_storage_3d")) {
                 insert.storageTexture.viewDimension = WGPUTextureViewDimension_3D;
                 format_or_sample_type sampletype = extractFormat(iden);
+            } else if (iden->symbol.NameView().starts_with("buffer")) {
+                if (varvis.var->As<tint::ast::Var>()
+                        ->declared_address_space->As<tint::ast::IdentifierExpression>()
+                        ->identifier->symbol.NameView()
+                        .starts_with("storage")) {
+                    if (varvis.var->As<tint::ast::Var>()
+                            ->declared_access->As<tint::ast::IdentifierExpression>()
+                            ->identifier->symbol.NameView()
+                            .starts_with("readonly")) {
+                        insert.buffer.type = WGPUBufferBindingType_ReadOnlyStorage;
+                    } else {
+                        insert.buffer.type = WGPUBufferBindingType_Storage;
+                    }
+                } else if (varvis.var->As<tint::ast::Var>()
+                               ->declared_address_space->As<tint::ast::IdentifierExpression>()
+                               ->identifier->symbol.NameView()
+                               .starts_with("uniform")) {
+                    insert.buffer.type = WGPUBufferBindingType_Uniform;
+                }
+                insert.buffer.minBindingSize = sem->Type()->As<tint::core::type::Reference>()->Size();
             } else if (iden->symbol.Name().starts_with("sampler")) {
-                
             }
-            const_cast<WGPUGlobalReflectionInfo*>(ret.globals)[globalInsertIndex++] = insert;
+            const_cast<WGPUGlobalReflectionInfo *>(ret.globals)[globalInsertIndex++] = insert;
+        }
+        ret.globalCount = globalsByName.size();
+
+        tint::inspector::Inspector insp(prog);
+        namespace ti = tint::inspector;
+
+        const tint::sem::Info &psem = prog.Sem();
+        std::unordered_map<std::string, std::pair<VertexFormat, uint32_t>> retattrmap;
+        for (size_t i = 0; i < insp.GetEntryPoints().size(); i++) {
+            if (insp.GetEntryPoints()[i].stage == ti::PipelineStage::kVertex) {
+                for (size_t j = 0; j < insp.GetEntryPoints()[i].input_variables.size(); j++) {
+                    // std::string name = insp.GetEntryPoints()[i].input_variables[j].name;
+                    std::string varname = insp.GetEntryPoints()[i].input_variables[j].variable_name;
+                    if (!insp.GetEntryPoints()[i].input_variables[j].attributes.location.has_value())
+                        continue;
+                    uint32_t location = insp.GetEntryPoints()[i].input_variables[j].attributes.location.value();
+                    // std::cout << name << " and " << varname << "\n";
+                    // std::cout << insp.GetEntryPoints()[i].input_variables[j].attributes.location.value() << ": ";
+                    // std::cout << (int)insp.GetEntryPoints()[i].input_variables[j]. << ", ";
+                    if (insp.GetEntryPoints()[i].input_variables[j].component_type == ti::ComponentType::kF32) {
+                        switch (insp.GetEntryPoints()[i].input_variables[j].composition_type) {
+                        case ti::CompositionType::kScalar: {
+                            retattrmap[varname] = {VertexFormat_Float32, location};
+                        } break;
+                        case ti::CompositionType::kVec2: {
+                            retattrmap[varname] = {VertexFormat_Float32x2, location};
+                        } break;
+                        case ti::CompositionType::kVec3: {
+                            retattrmap[varname] = {VertexFormat_Float32x3, location};
+                        } break;
+                        case ti::CompositionType::kVec4: {
+                            retattrmap[varname] = {VertexFormat_Float32x4, location};
+                        } break;
+                        case ti::CompositionType::kUnknown: {
+                            TRACELOG(LOG_ERROR, "Unknown composition type");
+                        } break;
+                        }
+                    } else if (insp.GetEntryPoints()[i].input_variables[j].component_type == ti::ComponentType::kU32) {
+                        switch (insp.GetEntryPoints()[i].input_variables[j].composition_type) {
+                        case ti::CompositionType::kScalar: {
+                            retattrmap[varname] = {VertexFormat_Uint32, location};
+                        } break;
+                        case ti::CompositionType::kVec2: {
+                            retattrmap[varname] = {VertexFormat_Uint32x2, location};
+                        } break;
+                        case ti::CompositionType::kVec3: {
+                            retattrmap[varname] = {VertexFormat_Uint32x3, location};
+                        } break;
+                        case ti::CompositionType::kVec4: {
+                            retattrmap[varname] = {VertexFormat_Uint32x4, location};
+                        } break;
+                        case ti::CompositionType::kUnknown: {
+                            TRACELOG(LOG_ERROR, "Unknown composition type");
+                        } break;
+                        }
+                    } else if (insp.GetEntryPoints()[i].input_variables[j].component_type == ti::ComponentType::kI32) {
+                        switch (insp.GetEntryPoints()[i].input_variables[j].composition_type) {
+                        case ti::CompositionType::kScalar: {
+                            retattrmap[varname] = {VertexFormat_Sint32, location};
+                        } break;
+                        case ti::CompositionType::kVec2: {
+                            retattrmap[varname] = {VertexFormat_Sint32x2, location};
+                        } break;
+                        case ti::CompositionType::kVec3: {
+                            retattrmap[varname] = {VertexFormat_Sint32x3, location};
+                        } break;
+                        case ti::CompositionType::kVec4: {
+                            retattrmap[varname] = {VertexFormat_Sint32x4, location};
+                        } break;
+                        case ti::CompositionType::kUnknown: {
+                            TRACELOG(LOG_ERROR, "Unknown composition type");
+                        } break;
+                        }
+                    } else if (insp.GetEntryPoints()[i].input_variables[j].component_type == ti::ComponentType::kF16) {
+                        switch (insp.GetEntryPoints()[i].input_variables[j].composition_type) {
+                        case ti::CompositionType::kScalar: {
+                            retattrmap[varname] = {VertexFormat_Float16, location};
+                        } break;
+                        case ti::CompositionType::kVec2: {
+                            retattrmap[varname] = {VertexFormat_Float16x2, location};
+                        } break;
+                        case ti::CompositionType::kVec3: {
+                            TRACELOG(LOG_ERROR, "That should not be possible: vec3<f16>");
+                        } break;
+                        case ti::CompositionType::kVec4: {
+                            retattrmap[varname] = {VertexFormat_Float16x4, location};
+                        } break;
+                        case ti::CompositionType::kUnknown: {
+                            TRACELOG(LOG_ERROR, "Unknown composition type");
+                        } break;
+                        }
+                    } else {
+                        TRACELOG(LOG_ERROR, "Unknown component type");
+                    }
+
+                    // std::cout << (int)insp.GetEntryPoints()[i].input_variables[j].composition_type << ", ";
+                }
+                // std::cout << "\n";
+            } else if (insp.GetEntryPoints()[i].stage == ti::PipelineStage::kFragment) {
+                const std::vector<ti::StageVariable> vec_outvariables = insp.GetEntryPoints()[i].output_variables;
+
+                for (auto &outvar : vec_outvariables) {
+                    uint32_t dim = ~0;
+                    format_or_sample_type type = format_or_sample_type::we_dont_know;
+
+                    switch (outvar.composition_type) {
+                    case ti::CompositionType::kVec4:
+                        dim = 4;
+                        break;
+                    case ti::CompositionType::kVec3:
+                        dim = 3;
+                        break;
+                    case ti::CompositionType::kVec2:
+                        dim = 2;
+                        break;
+                    case ti::CompositionType::kScalar:
+                        dim = 1;
+                        break;
+                    default:
+                        rg_trap();
+                    }
+                    switch (outvar.component_type) {
+                    case ti::ComponentType::kF32:
+                        type = format_or_sample_type::sample_f32;
+                        break;
+                    case ti::ComponentType::kU32:
+                        type = format_or_sample_type::sample_u32;
+                        break;
+                    default:
+                        rg_trap();
+                    }
+                    //retvalue.attachments.emplace_back(outvar.attributes.location.value_or(LOCATION_NOT_FOUND), type);
+                }
+            }
         }
 
-    
-        
     }
 
     else {
