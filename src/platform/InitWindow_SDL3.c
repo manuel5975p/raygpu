c22b6f482bd527e0f272eb819ed71c8cdc915dab
Peter0x44 <peter0x44@disroot.org>
2025-12-22 18:20:09 +0000

    Fix CI

--- src/platform/InitWindow_SDL3.c
+++ src/platform/InitWindow_SDL3.c
@@ -379,7 +379,8 @@ RGAPI void PollEvents_SDL3() {
         case SDL_EVENT_WINDOW_CLOSE_REQUESTED:{
             // TODO: Implement this properly for subwindows
             SDL_Window *window = SDL_GetWindowFromID(event.window.windowID);
-            g_renderstate.closeFlag = true;
+            RGWindowImpl* rgWindow = CreatedWindowMap_get(&g_renderstate.createdSubwindows, window);
+            if(rgWindow) rgWindow->closeRequestedFlag = true;
         }break;
         case SDL_EVENT_KEY_DOWN:{
             SDL_Window *window = SDL_GetWindowFromID(event.key.windowID);
